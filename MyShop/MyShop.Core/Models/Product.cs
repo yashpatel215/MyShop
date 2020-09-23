e15de600944d7e61527d7bdcd7b058be855fe417
Yash <yash.patel2152@gmail.com>
2020-09-23 16:47:55 -0400

    Product Model and Product InMemory Repository Built.

---
+++ MyShop/MyShop.Core/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Retrieving product id,name,etc
+namespace MyShop.Core.Models
+{
+    public class Product
+    {
+        [StringLength(20)]
+        [DisplayName("Product Name")]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Range(0,1000)]
+        public string Price { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+        //instantiate Class(Constructor)
+        public Product()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ MyShop/MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Add below library from Reference memory cache
+using System.Runtime.Caching;
+using MyShop.Core;
+using MyShop.Core.Models;
+using System.Diagnostics.Contracts;
+// InMemory Cache to Extract db
+namespace MyShop.DataAccess.InMemory
+{
+    public class ProductRepository
+    {
+        //Create Cache Memory by creating object
+        ObjectCache cache = MemoryCache.Default;
+        //Internal List of products.
+        List<Product> products;
+
+        //Constructor to initialize product repository
+        public ProductRepository()
+        {
+            products = cache["products"] as List<Product>;
+            if(products == null)
+            {
+                products = new List<Product>();
+            }
+        }
+        // method lets add products to repository but not save them.
+        public void Commit()
+        {
+            cache["products"] = products;
+        }
+        // method to Insert Product
+        public void  Insert(Product p)
+        {
+            products.Add(p);
+        }
+        // Method to update product.
+        public void Update(Product product)
+        {
+            
+            Product productToUpdate = products.Find(p => p.Id == product.Id);
+
+            if (productToUpdate != null)
+            {
+                productToUpdate = product;
+            }
+            else
+            {
+                throw new Exception("Product Not Found");
+            }
+        }
+
+        //To Find the product using ID.
+        public Product Find(string Id)
+        {
+            Product product = products.Find(p => p.Id == Id);
+
+            if (product != null)
+            {
+                return product;
+            }
+            else
+            {
+                throw new Exception("Product Not Found");
+            }
+        }
+        //Return list of products that can be queried
+        public IQueryable<Product> Collection()
+        {
+            return products.AsQueryable();
+        }
+
+        //Method to delete product
+        public void Delete(string Id)
+        {
+            Product productToDelete = products.Find(p => p.Id == Id);
+
+            if (productToDelete != null)
+            {
+                products.Remove(productToDelete);
+            }
+            else
+            {
+                throw new Exception("Product Not Found");
+            }
+        }
+    }
+}
