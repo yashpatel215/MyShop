c747d26f4fd205d59800fe872da4c1887f6011b3
Yash <yash.patel2152@gmail.com>
2020-09-27 10:44:05 -0400

    Product Manager Pages Build and Tested

--- MyShop/MyShop.Core/Models/Product.cs
+++ MyShop/MyShop.Core/Models/Product.cs
@@ -5,24 +5,27 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-// Retrieving product id,name,etc
+
 namespace MyShop.Core.Models
 {
     public class Product
     {
+        public string Id { get; set; }
+
         [StringLength(20)]
         [DisplayName("Product Name")]
-        public string Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        [Range(0,1000)]
-        public string Price { get; set; }
+
+        [Range(0, 1000)]
+        public decimal Price { get; set; }
         public string Category { get; set; }
         public string Image { get; set; }
-        //instantiate Class(Constructor)
+
         public Product()
         {
             this.Id = Guid.NewGuid().ToString();
         }
+
     }
 }
--- MyShop/MyShop.DataAccess.InMemory/ProductRepository.cs
+++ MyShop/MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -3,44 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//Add below library from Reference memory cache
 using System.Runtime.Caching;
-using MyShop.Core;
 using MyShop.Core.Models;
-using System.Diagnostics.Contracts;
-// InMemory Cache to Extract db
+
 namespace MyShop.DataAccess.InMemory
 {
     public class ProductRepository
     {
-        //Create Cache Memory by creating object
         ObjectCache cache = MemoryCache.Default;
-        //Internal List of products.
         List<Product> products;
 
-        //Constructor to initialize product repository
         public ProductRepository()
         {
             products = cache["products"] as List<Product>;
-            if(products == null)
+            if (products == null)
             {
                 products = new List<Product>();
             }
         }
-        // method lets add products to repository but not save them.
+
         public void Commit()
         {
             cache["products"] = products;
         }
-        // method to Insert Product
-        public void  Insert(Product p)
+
+        public void Insert(Product p)
         {
             products.Add(p);
         }
-        // Method to update product.
+
         public void Update(Product product)
         {
-            
             Product productToUpdate = products.Find(p => p.Id == product.Id);
 
             if (productToUpdate != null)
@@ -49,11 +42,10 @@ namespace MyShop.DataAccess.InMemory
             }
             else
             {
-                throw new Exception("Product Not Found");
+                throw new Exception("Product no found");
             }
         }
 
-        //To Find the product using ID.
         public Product Find(string Id)
         {
             Product product = products.Find(p => p.Id == Id);
@@ -64,16 +56,15 @@ namespace MyShop.DataAccess.InMemory
             }
             else
             {
-                throw new Exception("Product Not Found");
+                throw new Exception("Product no found");
             }
         }
-        //Return list of products that can be queried
+
         public IQueryable<Product> Collection()
         {
             return products.AsQueryable();
         }
 
-        //Method to delete product
         public void Delete(string Id)
         {
             Product productToDelete = products.Find(p => p.Id == Id);
@@ -84,7 +75,7 @@ namespace MyShop.DataAccess.InMemory
             }
             else
             {
-                throw new Exception("Product Not Found");
+                throw new Exception("Product no found");
             }
         }
     }
